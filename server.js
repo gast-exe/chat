d1c6d209a74cdecb831d91ce014584f60e586080
gast-exe <gast228@bigmir.net>
2023-03-25 18:39:42 +0200

    Add sockets

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const http = require('http');
 const fs = require('fs');
-
+const { Server } = require('socket.io');
 
 const readStaticFile = fileName => fs.readFileSync(path.join(__dirname, 'static', fileName), 'utf8');
 
@@ -21,3 +21,9 @@ const server = http.createServer((request, response) => {
 
 server.listen(3000);
 
+const io = new Server(server);
+
+io.on('connection', socket => {
+   console.log('a user connected. Id: ' + socket.id);
+   socket.on('new_message', message => console.log(message));
+}); 
