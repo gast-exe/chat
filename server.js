f4d3ff0c5351554e17822204e76dd77ef94e2136
gast-exe <gast228@bigmir.net>
2023-03-25 18:39:07 +0200

    add another queries

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ const path = require('path');
 const http = require('http');
 const fs = require('fs');
 
-const index = fs.readFileSync(path.join(__dirname, 'static', 'index.html'), 'utf8');
+
+const readStaticFile = fileName => fs.readFileSync(path.join(__dirname, 'static', fileName), 'utf8');
+
+const index = readStaticFile('index.html'),
+   style = readStaticFile('style.css'),
+   script = readStaticFile('script.js');
 
 const server = http.createServer((request, response) => {
    switch (request.url) {
       case '/': return response.end(index);
+      case '/style.css': return response.end(style);
+      case '/script.js': return response.end(script);
    }
    response.statusCode = 404;
    return response.end('Error 404');
 });
 
 server.listen(3000);
+
