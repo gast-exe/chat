8590bdfd5a5a991ae7668dccdafaa602a7f16123
gast-exe <gast228@bigmir.net>
2023-04-01 17:54:14 +0300

    fixes

--- static/script.js
+++ static/script.js
@@ -1,12 +1,12 @@
 const socket = io();
 const messages = document.getElementById('messages');
-const form = document.createElement('form');
-const input = document.createElement('input');
+const form = document.getElementById('form');
+const input = document.getElementById('input');
 
 form.addEventListener('submit', event => {
    event.preventDefault();
-   if(input.value){
-      socket.emit('new_message',input.value);
+   if (input.value) {
+      socket.emit('new_message', input.value);
       input.value = '';
    }
 });
