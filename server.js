f61a879cb6509afe10efd6ff33935a81758fea38
gast-exe <gast228@bigmir.net>
2023-04-08 18:24:51 +0300

    add register to backend + minor updates

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 const path = require('path');
 const http = require('http');
 const fs = require('fs');
+const db = require('./database');
 const { Server } = require('socket.io');
 
 const readStaticFile = fileName => fs.readFileSync(path.join(__dirname, 'static', fileName), 'utf8');
 
 const index = readStaticFile('index.html'),
    style = readStaticFile('style.css'),
-   script = readStaticFile('script.js');
+   script = readStaticFile('script.js'),
+   register = readStaticFile('register.html'),
+   auth = readStaticFile('auth.js');
 
 const server = http.createServer((request, response) => {
    switch (request.url) {
       case '/': return response.end(index);
       case '/style.css': return response.end(style);
       case '/script.js': return response.end(script);
+      case '/register.html': return response.end(register);
+      case '/auth.js': return response.end(auth);
+   }
+   if (request.method == 'POST') {
+      switch (request.url) {
+         case '/api/register': return registerUser(request, response);
+      }
    }
    response.statusCode = 404;
    return response.end('Error 404');
@@ -23,14 +33,34 @@ server.listen(3000);
 
 const io = new Server(server);
 
-io.on('connection', socket => {
+io.on('connection', async socket => {
    console.log('a user connected. Id: ' + socket.id);
    let nickname = 'admin';
+   const message = await db.getMessages();
+   socket.emit('all_messages', message);
+
    socket.on('new_message', message => {
-      console.log(message);
+      db.addMessage(1, message);
       io.emit('message', `${nickname}: ${message}`);
    });
-   socket.on('set_nick', nick => {
-      nickname = nick;
+});
+
+function registerUser(request, response) {
+   let data = '';
+   request.on('data', chunk => data += chunk);
+   request.on('end', async () => {
+      try {
+         const user = JSON.parse(data);
+         if (!user.login || !user.password) {
+            return response.end('Empty login or password')
+         }
+         if (await db.isUserExist(user.login)) {
+            return response.end('User already exists');  
+         }
+         await db.addUser(user);
+         response.end('Registers is successful');
+      } catch (err) {
+         console.log(err);
+      }
    });
-}); 
+}
